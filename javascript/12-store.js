45e9dea5ca170d32c2cd289858b9de01f9e65781
Abdulrahman Boda <127202241+AbdulrahmanSalah88@users.noreply.github.com>
2023-03-16 03:35:27 +0200

    new Change

--- javascript/12-store.js
+++ javascript/12-store.js
@@ -1,102 +1,99 @@
-const showitems = document.createElement("div")
-const btnPrimary = document.querySelectorAll(".btn-primary")
+const showitems = document.createElement("div");
+const btnPrimary = document.querySelectorAll(".btn-primary");
 
 // Calculate the => (total price)
 const updateTotalePrice = () => {
-    const itemconainer = document.querySelectorAll(".item-conainer")
+    const itemconainer = document.querySelectorAll(".item-conainer");
 
+    let totale = 0;
+    itemconainer.forEach((item) => {
+        const newPrice = Number(
+            item.getElementsByClassName("price")[0].innerText.replace("$", "")
+        );
 
-    let totale = 0
-    itemconainer.forEach(item => {
-        const newPrice = Number(item.getElementsByClassName("price")[0].innerText.replace("$", ""))
+        const counter = Number(
+            item.getElementsByClassName("input-of-product")[0].value
+        );
 
-        const counter = Number(item.getElementsByClassName("input-of-product")[0].value)
-
-
-
-        totale = totale + (newPrice * counter)
-
+        totale = totale + newPrice * counter;
     });
 
-    totalPrice.innerText = `${totale}$`
-}
-
+    totalPrice.innerText = `${totale}$`;
+};
 
 // whend I click inside (BlackScreen)
 zoonBlackScreen.addEventListener("click", (eo) => {
-    updateTotalePrice()
-})
+    updateTotalePrice();
+});
 // Whend I Change inside (BlackScreen)
 zoonBlackScreen.addEventListener("change", (eo) => {
-    updateTotalePrice()
-})
-
+    updateTotalePrice();
+});
 
 zoonBlackScreen.addEventListener("click", (eo) => {
-
     if (eo.target.classList.contains("btn-secondary")) {
-        eo.target.parentElement.remove()
-        updateTotalePrice()
+        eo.target.parentElement.remove();
+        updateTotalePrice();
 
-        const NameProducteInBlackScreen = eo.target.parentElement.getElementsByClassName("Name-product")[0].innerText
+        const NameProducteInBlackScreen =
+            eo.target.parentElement.getElementsByClassName("Name-product")[0]
+                .innerText;
 
-        const allcards = document.querySelectorAll(".card")
+        const allcards = document.querySelectorAll(".card");
 
-        allcards.forEach(item => {
-            const AllNameProductInMyWep = item.getElementsByClassName("card-title")[0].innerText
-
+        allcards.forEach((item) => {
+            const AllNameProductInMyWep =
+                item.getElementsByClassName("card-title")[0].innerText;
 
             if (NameProducteInBlackScreen == AllNameProductInMyWep) {
-                const undone = item.getElementsByClassName("btn btn-success")[0]
-                undone.classList.remove("btn-success")
-                undone.classList.add("btn-primary")
-                undone.removeAttribute("disabled")
-                undone.innerText = `Buy`
+                const undone = item.getElementsByClassName("btn btn-success")[0];
+                undone.classList.remove("btn-success");
+                undone.classList.add("btn-primary");
+                undone.removeAttribute("disabled");
+                undone.innerText = `Buy`;
                 // undone.
-
             }
-
         });
-
     }
+});
 
-})
-
-btnPrimary.forEach(item => {
+btnPrimary.forEach((item) => {
     item.addEventListener("click", (eo) => {
-
-        { //  Change Buttons from "Buy" to "✔ Done"
-            item.classList.remove("btn-primary")
-            item.classList.add("btn-success")
-            item.setAttribute("disabled", "")
-            item.innerHTML = `&#10004;  Done`
+        {
+            //  Change Buttons from "Buy" to "✔ Done"
+            item.classList.remove("btn-primary");
+            item.classList.add("btn-success");
+            item.setAttribute("disabled", "");
+            item.innerHTML = `&#10004;  Done`;
         }
 
-        {    // The congratulatory element
-            const congratulations = document.createElement("div")
-            body.prepend(congratulations)
-            congratulations.classList.add("cong-of-clinte")
-            congratulations.innerHTML = `  &#128525;	 مبروووووووك`
+        {
+            // The congratulatory element
+            const congratulations = document.createElement("div");
+            body.prepend(congratulations);
+            congratulations.classList.add("cong-of-clinte");
+            congratulations.innerHTML = `  &#128525;	 مبروووووووك`;
             setTimeout(() => {
-                congratulations.style.transform = `translateX(-100vw)`
+                congratulations.style.transform = `translateX(-100vw)`;
             }, 2000);
             setTimeout(() => {
-                congratulations.remove()
+                congratulations.remove();
             }, 4000);
         }
 
-        {   // create Elements show Producte
-            body.prepend(showitems)
-            showitems.classList.add("show-product")
-            showitems.innerHTML = `&#128722;        عرض المشتريات`
+        {
+            // create Elements show Producte
+            body.prepend(showitems);
+            showitems.classList.add("show-product");
+            showitems.innerHTML = `&#128722;        عرض المشتريات`;
         }
 
-        // The New Elements of Black Screen 
-        const cards = item.parentElement.parentElement.parentElement
-        const balackImg = cards.getElementsByTagName("img")[0].src
-        const NameProducte = cards.getElementsByClassName("card-title")[0].innerText
-        const priceProducte = cards.getElementsByClassName("price")[0].innerText
-
+        // The New Elements of Black Screen
+        const cards = item.parentElement.parentElement.parentElement;
+        const balackImg = cards.getElementsByTagName("img")[0].src;
+        const NameProducte =
+            cards.getElementsByClassName("card-title")[0].innerText;
+        const priceProducte = cards.getElementsByClassName("price")[0].innerText;
 
         const printInblackScreen = `
         <div dir="rtl" class="item-conainer">
@@ -121,27 +118,23 @@ btnPrimary.forEach(item => {
         <div class="btn btn-secondary">
             حذف
         </div>
-        `
-        zoonBlackScreen.innerHTML += printInblackScreen
+        `;
+        zoonBlackScreen.innerHTML += printInblackScreen;
 
-        updateTotalePrice()
-        console.group(updateTotalePrice)
-
-    })
+        updateTotalePrice();
+        console.group(updateTotalePrice);
+    });
 });
 
-
 // When Click on (close) in black Screen
-const closee = document.getElementById("close")
-const backScrren = document.querySelector(".back-screen")
+const closee = document.getElementById("close");
+const backScrren = document.querySelector(".back-screen");
 
 closee.addEventListener("click", (eo) => {
-    backScrren.style.transform = `translateX(-100vw)`
-})
+    backScrren.style.transform = `translateX(-100vw)`;
+});
 
 // When Click on (عرض المشتريات) to show black Screen
 showitems.addEventListener("click", (eo) => {
-    backScrren.style.transform = `translateX(0vw)`
-
-
-})
+    backScrren.style.transform = `translateX(0vw)`;
+});
--- javascript/section.js
+++ javascript/section.js
@@ -31,36 +31,40 @@
 // using Array & Math.random()   ( floor & trunc )
 // level 3 lesson 2 Video #5
 {
-  //     const Color = ["#390099", "#ffba08", "#f72585", "#48cae4", "#e63946"];
-  //     sectionbg.addEventListener("click" , (eo) => {
-  //       const  AllSection = document.querySelectorAll("section")
-  // const random = Math.floor(Math.random() * Color.length  )
-  // console.log(random)
-  // AllSection.forEach(item => {
-  //   item.style.backgroundColor = Color[random]
-  // });
-  //     })
+//       const Color = ["#390099", "#ffba08", "#f72585", "#48cae4", "#e63946"];
+//       sectionbg.addEventListener("click" , (eo) => {
+//         const  AllSection = document.querySelectorAll("section")
+        
+//         AllSection.forEach(item => {
+//   const random = Math.round(Math.random() * Color.length-1)
+//   item.style.backgroundColor = Color[random]
+// });
+//       })
+
 }
+
+
+
 
 // Section Change Background Click On "sectionbg"
 // using rgb & Math.random() return function
 // level 3 lesson 2 Video #6
 {
   // const retrgb = (eo) => {
-  //   return Math.round(Math.random() * 255)
+    // return Math.round(Math.random() * 255)
   // }
-  //   sectionbg.addEventListener("click" , (eo) => {
-  //     const AllSection = document.querySelectorAll("section")
-  //     const rgb1 = retrgb()
-  //     const rgb2 = retrgb()
-  //     const rgb3 = retrgb()
-  //     AllSection.forEach(item => {
-  //       console.log(rgb1,
-  //         rgb2,
-  //         rgb3);
-  //     item.style.backgroundColor = `rgb(${rgb1},${rgb2} ,${rgb3} )`
-  //     });
-  //   })
+    // sectionbg.addEventListener("click" , (eo) => {
+    //   const AllSection = document.querySelectorAll("section")
+    //   const rgb1 = retrgb()
+    //   const rgb2 = retrgb()
+    //   const rgb3 = retrgb()
+    //   AllSection.forEach(item => {
+    //     console.log(rgb1,
+    //       rgb2,
+    //       rgb3);
+    //   item.style.backgroundColor = `rgb(${rgb1},${rgb2} ,${rgb3} )`
+    //   });
+    // })
 }
 
 // Section Change Background Click On "sectionbg"
@@ -85,18 +89,24 @@
 
 }
 
+
+
+
+
+
+
 // Section Change Background Click On "sectionbg"
 // using hex &   using \ for & for in & for of & while & for do While & for (;;) \
 // level 3 lesson 2 Video
 {
 
-  const hx = () => {
-    return `#${Math.random().toString(16).slice(2, 8)}`
-  }
+  // const hx = () => {
+  //   return `#${Math.random().toString(16).slice(2, 8)}`
+  // }
 
-  sectionbg.addEventListener("click", (eo) => {
-    const Allsection = document.querySelectorAll("section")
-    const ret = hx()
+  // sectionbg.addEventListener("click", (eo) => {
+  //   const Allsection = document.querySelectorAll("section")
+  //   const ret = hx()
 
 
   // let i = 0
@@ -143,10 +153,11 @@
 
 
 
-    for (let i = 0; i < Allsection.length; i++) {
-      const item = Allsection[i];
-      item.style.backgroundColor = ret
+    // for (let i = 0; i < Allsection.length; i++) {
+    //   const item = Allsection[i];
+    //   item.style.backgroundColor = ret
     }
 
-  })
-}
+  // )
+  
+
